a377f7521661552b8655aeea752f14f4a2c6769f
Vashistha Iyer <iy3rvashistha@gmail.com>
2021-04-16 19:34:12 +0530

    Mute on Slack script

---
+++ scripts/muteOnSlack.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Mute on Slack
+// @namespace    https://capitalmind.in
+// @version      0.1
+// @description  Mute users on Capitalmind Slack
+// @author       Vashistha Iyer
+// @match        https://app.slack.com/client/T04NJNVHN/*
+// @icon         https://www.google.com/s2/favicons?domain=slack.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const usersToMute = ["U2AHEBASG", "U01CS0YC6PM"];
+
+  const muteQuery = usersToMute
+    .map((x) => `a[data-message-sender="${x}"]`)
+    .join(", ");
+
+  const observer = new MutationObserver(function (mutations) {
+    [...document.querySelectorAll('div[data-qa="message_container"]')]
+      .filter((x) => x.querySelectorAll(muteQuery).length)
+      .map((x) => {
+        x.style.display = "none";
+      });
+  });
+
+  observer.observe(document.querySelector("body"), {
+    subtree: true,
+    childList: true,
+    attributes: false,
+    characterData: false,
+    attributeOldValue: false,
+    characterDataOldValue: false,
+  });
+})();
